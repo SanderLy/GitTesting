57920b25069c460703fabc696b2c2504b8e1ce60
Lysander Adriel L. Gawaran <glysander22@gmail.com>
2016-01-24 19:35:54 +0800

    Added dictionary.txt

--- Shiritori/Shiritori/Form1.Designer.cs
+++ Shiritori/Shiritori/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Shiritori
 {
-    partial class Form1
+    partial class frmGame
     {
         /// <summary>
         /// Required designer variable.
@@ -28,33 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.txtword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btnOk
             // 
-            this.button1.Location = new System.Drawing.Point(95, 142);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnOk.Location = new System.Drawing.Point(191, 118);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(99, 40);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
-            // Form1
+            // txtword
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.txtword.Location = new System.Drawing.Point(69, 70);
+            this.txtword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtword.Name = "txtword";
+            this.txtword.Size = new System.Drawing.Size(375, 27);
+            this.txtword.TabIndex = 1;
+            this.txtword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtword_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter word:";
+            // 
+            // frmGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(912, 381);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtword);
+            this.Controls.Add(this.btnOk);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmGame";
+            this.Text = "The Beginning of The End";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmGame_FormClosed);
+            this.Load += new System.EventHandler(this.frmGame_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.TextBox txtword;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Shiritori/Shiritori/Form1.cs
+++ Shiritori/Shiritori/Form1.cs
@@ -7,14 +7,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Shiritori
 {
-    public partial class Form1 : Form
+    public partial class frmGame : Form
     {
-        public Form1()
+        string used_words = Application.StartupPath + "\\used.txt";
+        string dictionary = Application.StartupPath + "\\dictionary.txt";
+        public frmGame()
         {
+            
             InitializeComponent();
+           
         }
+
+     private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (txtword.TextLength != 0) 
+            {
+                bool word_played = File.ReadAllText(dictionary).Contains(txtword.Text);
+                if (word_played == true)
+                {
+                    bool word_used = File.ReadAllText(used_words).Contains(txtword.Text);
+                    if (word_used == false)
+                    {
+                        TextWriter writer = new StreamWriter(used_words, true);
+                        if (!File.Exists(used_words))
+                        {
+                            File.CreateText(used_words);
+                            writer.WriteLine(txtword.Text);
+                            writer.Close();
+                        }
+                        // <--- check if created: used words list -- >
+                        if (File.Exists(used_words))
+                        {
+                            writer.WriteLine(txtword.Text);
+                            writer.Close();
+                        }
+                    }
+                    // <--- check if word is already used -- >
+                    else
+                    {
+                        MessageBox.Show("Word is already used. Try something else.", "Message");
+                    }
+                }
+                // <--- check word inside the dictionary -- >
+                else 
+                {
+                    MessageBox.Show("Word does not exist in the dictionary.", "Message");
+                }
+             }
+            // <--- check if the textbox is empty -- >
+            else
+            {
+                MessageBox.Show("Please enter a word", "Message");
+            }
+        }
+
+            
+
+     private void frmGame_FormClosed(object sender, FormClosedEventArgs e)
+     {
+         File.Delete(used_words);
+     }
+
+     private void txtword_KeyPress(object sender, KeyPressEventArgs e)
+     {
+         if (e.KeyChar == (char)Keys.Return)
+         {
+             btnOk.PerformClick();
+         }
+     }
+
+     private void frmGame_Load(object sender, EventArgs e)
+     {
+         if (!File.Exists(used_words))
+         {
+             File.CreateText(used_words);
+             
+         }
+     }
     }
 }
