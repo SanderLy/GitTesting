c836836bddab36c7bb035cc28bc7e703fb88b153
Lysander Adriel L. Gawaran <glysander22@gmail.com>
2016-02-27 21:12:11 +0800

    back button and panels are now working properly

--- Shiritori/Shiritori/MainMenu.Designer.cs
+++ Shiritori/Shiritori/MainMenu.Designer.cs
@@ -31,25 +31,29 @@
             this.btnTPlayer = new System.Windows.Forms.Button();
             this.btnSPlayer = new System.Windows.Forms.Button();
             this.pnlMenu = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.btnHow = new System.Windows.Forms.Button();
             this.btnPlay = new System.Windows.Forms.Button();
             this.pnlPlayers = new System.Windows.Forms.Panel();
             this.pnlMode = new System.Windows.Forms.Panel();
             this.btnHighScore = new System.Windows.Forms.Button();
             this.btnLastMan = new System.Windows.Forms.Button();
-            this.pnlDifficulty = new System.Windows.Forms.Panel();
-            this.btnHard = new System.Windows.Forms.Button();
-            this.btnMedium = new System.Windows.Forms.Button();
-            this.btnEasy = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlNet = new System.Windows.Forms.Panel();
             this.btnHost = new System.Windows.Forms.Button();
             this.btnClient = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.pnlHow = new System.Windows.Forms.Panel();
+            this.lblHow = new System.Windows.Forms.Label();
+            this.btnEasy = new System.Windows.Forms.Button();
+            this.btnMedium = new System.Windows.Forms.Button();
+            this.btnHard = new System.Windows.Forms.Button();
+            this.pnlDifficulty = new System.Windows.Forms.Panel();
             this.pnlMenu.SuspendLayout();
             this.pnlPlayers.SuspendLayout();
             this.pnlMode.SuspendLayout();
+            this.pnlNet.SuspendLayout();
+            this.pnlHow.SuspendLayout();
             this.pnlDifficulty.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnTPlayer
@@ -61,6 +65,7 @@
             this.btnTPlayer.TabStop = false;
             this.btnTPlayer.Text = "Two Player";
             this.btnTPlayer.UseVisualStyleBackColor = true;
+            this.btnTPlayer.Click += new System.EventHandler(this.btnTPlayer_Click);
             // 
             // btnSPlayer
             // 
@@ -71,36 +76,38 @@
             this.btnSPlayer.TabStop = false;
             this.btnSPlayer.Text = "Single Player";
             this.btnSPlayer.UseVisualStyleBackColor = true;
+            this.btnSPlayer.Click += new System.EventHandler(this.btnSPlayer_Click);
             // 
             // pnlMenu
             // 
-            this.pnlMenu.Controls.Add(this.button2);
-            this.pnlMenu.Controls.Add(this.button1);
+            this.pnlMenu.Controls.Add(this.btnQuit);
+            this.pnlMenu.Controls.Add(this.btnHow);
             this.pnlMenu.Controls.Add(this.btnPlay);
             this.pnlMenu.Location = new System.Drawing.Point(12, 12);
             this.pnlMenu.Name = "pnlMenu";
             this.pnlMenu.Size = new System.Drawing.Size(200, 96);
             this.pnlMenu.TabIndex = 3;
             // 
-            // button2
+            // btnQuit
             // 
-            this.button2.Location = new System.Drawing.Point(57, 62);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.TabStop = false;
-            this.button2.Text = "Quit";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnQuit.Location = new System.Drawing.Point(57, 62);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 2;
+            this.btnQuit.TabStop = false;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnHow
             // 
-            this.button1.Location = new System.Drawing.Point(57, 33);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.TabStop = false;
-            this.button1.Text = "How to Play";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnHow.Location = new System.Drawing.Point(57, 33);
+            this.btnHow.Name = "btnHow";
+            this.btnHow.Size = new System.Drawing.Size(75, 23);
+            this.btnHow.TabIndex = 1;
+            this.btnHow.TabStop = false;
+            this.btnHow.Text = "How to Play";
+            this.btnHow.UseVisualStyleBackColor = true;
+            this.btnHow.Click += new System.EventHandler(this.btnHow_Click);
             // 
             // btnPlay
             // 
@@ -111,6 +118,7 @@
             this.btnPlay.TabStop = false;
             this.btnPlay.Text = "Play";
             this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // pnlPlayers
             // 
@@ -120,6 +128,7 @@
             this.pnlPlayers.Name = "pnlPlayers";
             this.pnlPlayers.Size = new System.Drawing.Size(200, 96);
             this.pnlPlayers.TabIndex = 3;
+            this.pnlPlayers.Visible = false;
             // 
             // pnlMode
             // 
@@ -129,6 +138,7 @@
             this.pnlMode.Name = "pnlMode";
             this.pnlMode.Size = new System.Drawing.Size(200, 96);
             this.pnlMode.TabIndex = 3;
+            this.pnlMode.Visible = false;
             // 
             // btnHighScore
             // 
@@ -139,6 +149,7 @@
             this.btnHighScore.TabStop = false;
             this.btnHighScore.Text = "Highest Scorer";
             this.btnHighScore.UseVisualStyleBackColor = true;
+            this.btnHighScore.Click += new System.EventHandler(this.btnHighScore_Click);
             // 
             // btnLastMan
             // 
@@ -149,55 +160,17 @@
             this.btnLastMan.TabStop = false;
             this.btnLastMan.Text = "Last Man Standing";
             this.btnLastMan.UseVisualStyleBackColor = true;
+            this.btnLastMan.Click += new System.EventHandler(this.btnLastMan_Click);
             // 
-            // pnlDifficulty
+            // pnlNet
             // 
-            this.pnlDifficulty.Controls.Add(this.btnHard);
-            this.pnlDifficulty.Controls.Add(this.btnMedium);
-            this.pnlDifficulty.Controls.Add(this.btnEasy);
-            this.pnlDifficulty.Location = new System.Drawing.Point(228, 12);
-            this.pnlDifficulty.Name = "pnlDifficulty";
-            this.pnlDifficulty.Size = new System.Drawing.Size(200, 96);
-            this.pnlDifficulty.TabIndex = 4;
-            // 
-            // btnHard
-            // 
-            this.btnHard.Location = new System.Drawing.Point(57, 62);
-            this.btnHard.Name = "btnHard";
-            this.btnHard.Size = new System.Drawing.Size(75, 23);
-            this.btnHard.TabIndex = 2;
-            this.btnHard.TabStop = false;
-            this.btnHard.Text = "Hard";
-            this.btnHard.UseVisualStyleBackColor = true;
-            // 
-            // btnMedium
-            // 
-            this.btnMedium.Location = new System.Drawing.Point(57, 33);
-            this.btnMedium.Name = "btnMedium";
-            this.btnMedium.Size = new System.Drawing.Size(75, 23);
-            this.btnMedium.TabIndex = 1;
-            this.btnMedium.TabStop = false;
-            this.btnMedium.Text = "Medium";
-            this.btnMedium.UseVisualStyleBackColor = true;
-            // 
-            // btnEasy
-            // 
-            this.btnEasy.Location = new System.Drawing.Point(57, 3);
-            this.btnEasy.Name = "btnEasy";
-            this.btnEasy.Size = new System.Drawing.Size(75, 23);
-            this.btnEasy.TabIndex = 0;
-            this.btnEasy.TabStop = false;
-            this.btnEasy.Text = "Easy";
-            this.btnEasy.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.btnHost);
-            this.panel1.Controls.Add(this.btnClient);
-            this.panel1.Location = new System.Drawing.Point(228, 117);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 96);
-            this.panel1.TabIndex = 4;
+            this.pnlNet.Controls.Add(this.btnHost);
+            this.pnlNet.Controls.Add(this.btnClient);
+            this.pnlNet.Location = new System.Drawing.Point(228, 117);
+            this.pnlNet.Name = "pnlNet";
+            this.pnlNet.Size = new System.Drawing.Size(200, 96);
+            this.pnlNet.TabIndex = 4;
+            this.pnlNet.Visible = false;
             // 
             // btnHost
             // 
@@ -208,6 +181,7 @@
             this.btnHost.TabStop = false;
             this.btnHost.Text = "Play as Host";
             this.btnHost.UseVisualStyleBackColor = true;
+            this.btnHost.Click += new System.EventHandler(this.btnHost_Click);
             // 
             // btnClient
             // 
@@ -218,24 +192,102 @@
             this.btnClient.TabStop = false;
             this.btnClient.Text = "Play as Client";
             this.btnClient.UseVisualStyleBackColor = true;
+            this.btnClient.Click += new System.EventHandler(this.btnClient_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(242, 236);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.Text = "<- Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // pnlHow
+            // 
+            this.pnlHow.Controls.Add(this.lblHow);
+            this.pnlHow.Location = new System.Drawing.Point(12, 318);
+            this.pnlHow.Name = "pnlHow";
+            this.pnlHow.Size = new System.Drawing.Size(200, 100);
+            this.pnlHow.TabIndex = 6;
+            this.pnlHow.Visible = false;
+            // 
+            // lblHow
+            // 
+            this.lblHow.AutoSize = true;
+            this.lblHow.Location = new System.Drawing.Point(34, 21);
+            this.lblHow.Name = "lblHow";
+            this.lblHow.Size = new System.Drawing.Size(64, 13);
+            this.lblHow.TabIndex = 0;
+            this.lblHow.Text = "How to Play";
+            // 
+            // btnEasy
+            // 
+            this.btnEasy.Location = new System.Drawing.Point(57, 3);
+            this.btnEasy.Name = "btnEasy";
+            this.btnEasy.Size = new System.Drawing.Size(75, 23);
+            this.btnEasy.TabIndex = 0;
+            this.btnEasy.TabStop = false;
+            this.btnEasy.Text = "Easy";
+            this.btnEasy.UseVisualStyleBackColor = true;
+            this.btnEasy.Click += new System.EventHandler(this.btnEasy_Click);
+            // 
+            // btnMedium
+            // 
+            this.btnMedium.Location = new System.Drawing.Point(57, 33);
+            this.btnMedium.Name = "btnMedium";
+            this.btnMedium.Size = new System.Drawing.Size(75, 23);
+            this.btnMedium.TabIndex = 1;
+            this.btnMedium.TabStop = false;
+            this.btnMedium.Text = "Medium";
+            this.btnMedium.UseVisualStyleBackColor = true;
+            this.btnMedium.Click += new System.EventHandler(this.btnMedium_Click);
+            // 
+            // btnHard
+            // 
+            this.btnHard.Location = new System.Drawing.Point(57, 62);
+            this.btnHard.Name = "btnHard";
+            this.btnHard.Size = new System.Drawing.Size(75, 23);
+            this.btnHard.TabIndex = 2;
+            this.btnHard.TabStop = false;
+            this.btnHard.Text = "Hard";
+            this.btnHard.UseVisualStyleBackColor = true;
+            this.btnHard.Click += new System.EventHandler(this.btnHard_Click);
+            // 
+            // pnlDifficulty
+            // 
+            this.pnlDifficulty.Controls.Add(this.btnHard);
+            this.pnlDifficulty.Controls.Add(this.btnMedium);
+            this.pnlDifficulty.Controls.Add(this.btnEasy);
+            this.pnlDifficulty.Location = new System.Drawing.Point(228, 12);
+            this.pnlDifficulty.Name = "pnlDifficulty";
+            this.pnlDifficulty.Size = new System.Drawing.Size(200, 96);
+            this.pnlDifficulty.TabIndex = 4;
+            this.pnlDifficulty.Visible = false;
             // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlHow);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.pnlNet);
             this.Controls.Add(this.pnlDifficulty);
             this.Controls.Add(this.pnlMode);
             this.Controls.Add(this.pnlPlayers);
             this.Controls.Add(this.pnlMenu);
             this.Name = "MainMenu";
             this.Text = "MainMenu";
+            this.Load += new System.EventHandler(this.MainMenu_Load);
             this.pnlMenu.ResumeLayout(false);
             this.pnlPlayers.ResumeLayout(false);
             this.pnlMode.ResumeLayout(false);
+            this.pnlNet.ResumeLayout(false);
+            this.pnlHow.ResumeLayout(false);
+            this.pnlHow.PerformLayout();
             this.pnlDifficulty.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -246,18 +298,21 @@
         private System.Windows.Forms.Button btnSPlayer;
         private System.Windows.Forms.Panel pnlMenu;
         private System.Windows.Forms.Button btnPlay;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Button btnHow;
         private System.Windows.Forms.Panel pnlPlayers;
         private System.Windows.Forms.Panel pnlMode;
         private System.Windows.Forms.Button btnHighScore;
         private System.Windows.Forms.Button btnLastMan;
-        private System.Windows.Forms.Panel pnlDifficulty;
-        private System.Windows.Forms.Button btnHard;
-        private System.Windows.Forms.Button btnMedium;
-        private System.Windows.Forms.Button btnEasy;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlNet;
         private System.Windows.Forms.Button btnHost;
         private System.Windows.Forms.Button btnClient;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Panel pnlHow;
+        private System.Windows.Forms.Label lblHow;
+        private System.Windows.Forms.Button btnEasy;
+        private System.Windows.Forms.Button btnMedium;
+        private System.Windows.Forms.Button btnHard;
+        private System.Windows.Forms.Panel pnlDifficulty;
     }
 }
--- Shiritori/Shiritori/MainMenu.cs
+++ Shiritori/Shiritori/MainMenu.cs
@@ -14,12 +14,128 @@ namespace Shiritori
 {
     public partial class MainMenu : Form
     {
-        
-        Form game = new mGame();
+        bool single, two, Hscore, LMan;
         public MainMenu()
         {
             InitializeComponent();
         }
 
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+            single = false;
+            two = false;
+            Hscore = false;
+            LMan = false;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            pnlMenu.Visible = false;
+            pnlPlayers.Visible = true;
+
+        }
+
+        private void btnSPlayer_Click(object sender, EventArgs e)
+        {
+            pnlPlayers.Visible = false;
+            pnlMode.Visible = true;
+            two = false;
+            single = true;
+        }
+
+        private void btnTPlayer_Click(object sender, EventArgs e)
+        {
+            pnlPlayers.Visible = false;
+            pnlMode.Visible = true;
+            single = false;
+            two = true;
+        }
+
+        private void btnHighScore_Click(object sender, EventArgs e)
+        {
+            pnlMode.Visible = false;
+            if(single == true)
+            {
+                pnlDifficulty.Visible = true;
+            }
+            if(two == true)
+            {
+                pnlNet.Visible = true;
+            }
+        }
+
+        private void btnLastMan_Click(object sender, EventArgs e)
+        {
+            pnlMode.Visible = false;
+            if (single == true)
+            {
+                pnlDifficulty.Visible = true;
+            }
+            if (two == true)
+            {
+                pnlNet.Visible = true;
+            }
+        }
+
+        private void btnEasy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMedium_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHard_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClient_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHost_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if(pnlMenu.Visible == false && pnlPlayers.Visible ==true)
+            {
+                pnlMenu.Visible = true;
+                pnlPlayers.Visible = false;
+            }
+            else if (pnlPlayers.Visible == false && pnlMode.Visible == true)
+            {
+                pnlPlayers.Visible = true;
+                pnlMode.Visible = false;
+            }
+            else if(pnlMode.Visible == false && pnlDifficulty.Visible == true)
+            {
+                pnlMode.Visible = true;
+                pnlDifficulty.Visible = false;
+            }
+            else if (pnlMode.Visible == false && pnlNet.Visible == true)
+            {
+                pnlMode.Visible = true;
+                pnlNet.Visible = false;
+            }
+          
+            else if (pnlMenu.Visible == false && pnlHow.Visible == true)
+            {
+                pnlMenu.Visible = true;
+                pnlHow.Visible = false;
+            }
+        }
+
+        private void btnHow_Click(object sender, EventArgs e)
+        {
+            pnlMenu.Visible = false;
+            pnlHow.Visible = true;
+        }
     }
 }
